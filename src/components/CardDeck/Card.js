0bc385d1c4447b1ecebc856286c9fa096e074a94
3dit <scottmwhill@gmail.com>
2023-09-02 15:30:56 -0500

    fixed cards

--- src/components/CardDeck/Card.js
+++ src/components/CardDeck/Card.js
@@ -1,22 +1,25 @@
-import Panel from "../Panel";
 import { BsFillSuitClubFill } from "react-icons/bs";
 import { BsFillSuitHeartFill } from "react-icons/bs";
 import { BsFillSuitSpadeFill } from "react-icons/bs";
 import { BsFillSuitDiamondFill } from "react-icons/bs";
 
 function Card({ suit, rank }) {
-console.log('CARD#####', suit, rank)
+    //console.log('CARD#####', suit, rank)
     const suitsIcons = {
-        'club': (<BsFillSuitClubFill className="ml-1"/>),
-        'heart': (<BsFillSuitHeartFill className="ml-1"/>),
-        'spade': (<BsFillSuitSpadeFill className="ml-1"/>),
-        'diamond': (<BsFillSuitDiamondFill className="ml-1"/>),
+        'club': (<BsFillSuitClubFill className="ml-1" />),
+        'heart': (<BsFillSuitHeartFill className="ml-1" />),
+        'spade': (<BsFillSuitSpadeFill className="ml-1" />),
+        'diamond': (<BsFillSuitDiamondFill className="ml-1" />),
     }
 
     const getRankExpression = (rank) => {
-        if(rank<=10) return `${rank}`;
-        const rankMap = { '11': 'J', '12': 'Q', '13': 'K', '14': 'A'};
+        if (rank <= 10) return `${rank}`;
+        const rankMap = { '11': 'J', '12': 'Q', '13': 'K', '14': 'A' };
         return rankMap[`${rank}`];
+    }
+
+    const getCardColor = (suit) => {
+        return (suit === 'heart' || suit === 'diamond') ? 'red' : 'black'
     }
 
     const rankAndSuit = {
@@ -26,10 +29,9 @@ console.log('CARD#####', suit, rank)
         'diamond': (<div className="cardsuit">{suitsIcons[suit]}</div>),
     }
 
-    
-    return (
 
-        <div className="border-[.2rem] border-gray-400 rounded m-2 p-[.5rem] w-[6.5rem] h-[9rem] rounded-b-large drop-shadow-lg">
+    return (
+        <div className="border-[.2rem] border-gray-400 rounded m-2 p-[.5rem] w-[6.5rem] h-[9rem] rounded-b-large drop-shadow-lg" style={{ 'color': getCardColor(suit) }}>
 
             <div className="flex flex-col h-[8rem] justify-between">
                 <div className="flex flex-row justify-end scale scale-[1.5] mr-[.8rem]">
--- src/pages/CardDeckReducerPage.js
+++ src/pages/CardDeckReducerPage.js
@@ -1,34 +1,76 @@
 import Card from "../components/CardDeck/Card";
-import { useReducer } from 'react';
+import { useReducer, useEffect } from 'react';
 import Button from "../components/Button";
 import { produce } from "immer";
 
 const RESET_DECK = 'reset-deck';
 const ADD_CARD = 'add-card';
+const SET_DECK = 'set-deck';
 
 const suits = ['diamond', 'club', 'heart', 'spade'];
+
+const shouldUseImmerProducer = false;
+
+function findDuplicates(cards, source) {
+    for(let i=0;i<cards.length;i++) {
+        let filteredDeck = cards.filter((card, index) => {
+            return index !== i;
+        });
+        let anyDuplicates = filteredDeck.filter((card, index) => {
+            return cards[i].suit === card.suit && cards[i].rank === card.rank;
+        });
+        if(anyDuplicates.length) {
+            console.log(`${source}: DUPLICATE!`, cards[i], anyDuplicates);
+            alert(`duplicate for ${source}`)
+        }
+    }
+}
+
 const reducer = (state, action) => {
-    switch(action.type)
-    {
+    switch (action.type) {
         case RESET_DECK:
             state.cardDeck = generateDeck();
             state.dealtCards = [];
             return;
         case ADD_CARD:
             const updatedDeck = [...state.cardDeck];
-            let card = updatedDeck.splice(0,1)[0];
+            let card = updatedDeck.splice(0, 1)[0];
             //let card = state.cardDeck.splice(0,1)[0];
             //state.dealtCards = [...dealtCards, card];
             state.dealtCards = [...state.dealtCards, card];
             state.cardDeck = updatedDeck;
             //setFullDeck(updatedDeck);
+            findDuplicates(state.dealtCards, 'reducer ADD_CARD');
             return
         default:
             return state;
     }
 }
 
+const reducer_no_immer = (state, action) => {
+    switch (action.type) {
+        case RESET_DECK:
+            return { ...state, cardDeck:generateDeck(), dealtCards: [] };
+        case ADD_CARD:
+            const updatedDeck = [...state.cardDeck];
+            let card = updatedDeck.splice(0, 1)[0];
+            console.log('DECK COUNT NOW ', updatedDeck.length);
+            const newDealtCards = [...state.dealtCards, card];
+            findDuplicates(newDealtCards, 'reducer(no immer)');
+            return { ...state, dealtCards: newDealtCards, cardDeck: updatedDeck };
+        case SET_DECK:
+            let newState = { ...state, cardDeck: action.payload.newDeck };
+            console.log('CARDS REMAINING', newState.cardDeck.length);
+            return newState;
+
+        default:
+            return state;
+    }
+}
+
 const generateDeck = () => {
+    console.log('GENERATING NEW DECK');
+
     let sortedCardDeck = [];
     for (let i = 0; i < 4; i++) {
         let suit = suits[i];
@@ -38,34 +80,61 @@ const generateDeck = () => {
         }
     }
     let randomDeck = [];
-    for(let i=0;i<52;i++) {
-        let randomIndex = parseInt(sortedCardDeck.length * Math.random());
-        //console.log(`selecting index ${randomIndex} for deck with ${sortedCardDeck.length} left.`);
-        let card = sortedCardDeck.splice(randomIndex,1)[0];
-        randomDeck.push(card); 
-        //console.log('added ', card.suit, card.rank);
+    for (let i = 0; i < 52; i++) {
+        let nextCardIndex = parseInt(sortedCardDeck.length * Math.random());
+        
+        let card = sortedCardDeck.splice(nextCardIndex, 1)[0];
+
+        //let card = sortedCardDeck[nextCardIndex];
+        //sortedCardDeck = sortedCardDeck.filter((object, index) => index != 0);
+
+        randomDeck.push(card);
     }
 
     console.log('Deck Length', randomDeck.length);
+    console.log('RANDOM DECK', randomDeck);
+    // for(let i=0;i<randomDeck.length;i++) {
+    //     let filteredDeck = randomDeck.filter((card, index) => {
+    //         return index !== i;
+    //     });
+    //     let anyDuplicates = filteredDeck.filter((card, index) => {
+    //         return randomDeck[i].suit === card.suit && randomDeck[i].rank === card.rank;
+    //     });
+    //     if(anyDuplicates.length) console.log('DUPLICATE!', randomDeck[i], anyDuplicates);
+    // }
+    findDuplicates(randomDeck, 'generateDeck');
     return randomDeck;
-    console.log('RANDOM', randomDeck);
 }
 
-
+const initialCardDeck = generateDeck();
 
 function CardDeckReducerPage() {
-    
-    const [state, dispatch] = useReducer(produce(reducer), {
+
+    let reducerParam;
+    if(shouldUseImmerProducer === true) {
+        //producer from immer uses simpler reducer function
+        reducerParam = produce(reducer);
+    } else {
+        //standard approach w/o using immer shortcut
+        reducerParam = reducer_no_immer;
+    }
+
+
+    //NOTE: for below, when using "cardDeck: generateDeck()", the generateDeck() function is called for
+    //every component render/update, but it appears only the initial call is used for default data. In anycase,
+    //this is not efficient so use external function called once to avoid recomputing deck constantly
+    const [state, dispatch] = useReducer(reducerParam, {
         dealtCards: [],
-        cardDeck: generateDeck()
+        //cardDeck: generateDeck()
+        cardDeck: initialCardDeck
     });
-    
+
     const handleClick = () => {
         // const rank = 2 + parseInt(Math.random() * 12);
         // const suitIndex = parseInt(Math.random() * 4);
         // const suit = suits[suitIndex];
-        if(state.cardDeck.length===0) return;
-        dispatch({type: ADD_CARD});
+        if (state.cardDeck.length === 0) return;
+        dispatch({ type: ADD_CARD });
     }
 
     const handleReset = () => {
